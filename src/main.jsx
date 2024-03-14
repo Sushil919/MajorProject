4592b56441da573c627a00192bc12311b17bbca4
Sushil <sushilkumar811544@gmail.com>
2024-03-14 13:25:34 +0530

    setup to main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+export const Context = createContext({isAuthorized: false}); //Context hook for state management  => react/redux can also be used
+
+const AppWrapper = () => {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [user, setUser] = useState({});
+
+  return (
+    <Context.Provider value ={{ isAuthorized, setIsAuthorized, user, setUser }}>
+      <App />
+    </Context.Provider>
+  );
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppWrapper />
   </React.StrictMode>,
-)
+);
